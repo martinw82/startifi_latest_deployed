e7686e60cf745eda162de8ffea23aa28a39e93cb
martinw82 <139254858+martinw82@users.noreply.github.com>
2025-06-30 17:33:49 +0100

    Updated AdminDashboardPage.tsx

--- src/pages/AdminDashboardPage.tsx
+++ src/pages/AdminDashboardPage.tsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { useNavigate } from 'react-router-dom';
 import {
-  Shield, User, Clock, CheckCircle, XCircle, Package, Users, AlertCircle, Mail,
+  Shield, User, Clock, CheckCircle, XCircle, Package, Users, AlertCircle,
   DollarSign,
   TrendingUp,
   Eye,
