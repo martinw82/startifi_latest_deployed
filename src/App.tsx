dbb4d4ecc0a8eb265065d6462f6b9bc64ab7ba46
martinw82 <139254858+martinw82@users.noreply.github.com>
2025-06-30 15:20:35 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // src/App.tsx
-import React, { createContext } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Header } from './components/layout/Header';
 import { Footer } from './components/layout/Footer';
