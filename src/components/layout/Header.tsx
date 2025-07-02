2235ef3d8d8afcdeb4df6c5d47bd89e67c40c46a
martinw82 <139254858+martinw82@users.noreply.github.com>
2025-07-02 21:22:29 +0100

    Updated Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -76,7 +76,7 @@ export const Header: React.FC = () => {
           {/* Logo */}
           <Link to="/">
             <motion.div
-              className="flex items-center space-x-2 cursor-pointer"
+              className="flex items-center space-x-2 cursor-pointer pt-2.5"
               whileHover={{ scale: 1.05 }}
             >
               <img
